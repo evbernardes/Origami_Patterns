902221d6b45b4bcfc137e7783551fdd865836ad1
evbernardes <s.evandro@hotmail.com>
2019-08-30 16:30:57 +0200

    Waterbomb/Magic ball selection now works independently for first and last lines

--- OrigamiPatterns/Waterbomb.py
+++ OrigamiPatterns/Waterbomb.py
@@ -8,6 +8,9 @@ import inkex
 from Path import Path
 from Pattern import Pattern
 
+# TODO:
+# Add fractional column number option
+
 
 class Waterbomb(Pattern):
     
@@ -19,7 +22,17 @@ class Waterbomb(Pattern):
         self.OptionParser.add_option("-p", "--pattern",
                                      action="store", type="string",
                                      dest="pattern", default="waterbomb",
-                                     help="Origami pattern")      
+                                     help="Origami pattern")
+
+        self.OptionParser.add_option("", "--pattern_first_line",
+                                     action="store", type="string",
+                                     dest="pattern_first_line", default="waterbomb",
+                                     help="Origami pattern")
+
+        self.OptionParser.add_option("", "--pattern_last_line",
+                                     action="store", type="string",
+                                     dest="pattern_last_line", default="waterbomb",
+                                     help="Origami pattern")
         
         self.OptionParser.add_option("", "--lines",
                                      action="store", type="int",
@@ -50,7 +63,8 @@ class Waterbomb(Pattern):
         cols = self.options.columns
         lines = self.options.lines
         phase_shift = self.options.phase_shift
-        pattern = self.options.pattern
+        pattern_first_line = self.options.pattern_first_line
+        pattern_last_line = self.options.pattern_last_line
 
         # create vertices
         vertex_line_types = [[Path(((i / 2.) * length, 0), style='p', radius=vertex_radius) for i in range(2*cols + 1)],
@@ -59,7 +73,7 @@ class Waterbomb(Pattern):
 
         vertices = []
         for i in range(2*lines + 1):
-            if i % 2 == 0 or (i == 1 or i == 2*lines - 1) and pattern == 'magic_ball':
+            if i % 2 == 0 or (i == 1 or i == 2*lines - 1) and pattern_first_line == 'magic_ball':
                 type = 0
             elif(i/2 + phase_shift) % 2 == 0:
                 type = 1
@@ -69,14 +83,27 @@ class Waterbomb(Pattern):
 
         # create a list for the horizontal creases and another for the vertical creases
         # alternate strokes to minimize laser cutter path
-        corr = length/2 if pattern == 'magic_ball' else 0
+        corr_fist_line = length/2 if pattern_first_line == 'magic_ball' else 0
+        corr_last_line = length/2 if pattern_last_line == 'magic_ball' else 0
         grid = [Path.generate_hgrid([0, length*cols],    [0,      length*lines],  lines, 'm'),
-                Path.generate_vgrid([0, length*cols], [corr, length*lines-corr], 2*cols, 'm')]
-        if pattern == 'magic_ball':
-            vgrid_a = Path.generate_vgrid([0, length*cols], [0, length/2], 2*cols, 'v')
-            vgrid_b = Path.list_add(vgrid_a, (0, (lines-0.5)*length))
+                Path.generate_vgrid([0, length*cols], [corr_fist_line, length*lines-corr_last_line], 2*cols, 'm')]
+        # if corr_fist_line == 'magic_ball':
+        #     vgrid_a = Path.generate_vgrid([0, length*cols], [0, length/2], 2*cols, 'v')
+        #     vgrid_b = Path.list_add(vgrid_a, (0, (lines-0.5)*length))
+        #     grid[1] = [[vgrid_a[i], grid[1][i], vgrid_b[i]] if i % 2 == 0 else
+        #                [vgrid_b[i], grid[1][i], vgrid_a[i]] for i in range(len(grid[1]))]
+
+        vgrid_a = Path.generate_vgrid([0, length * cols], [0, length / 2], 2 * cols, 'v')
+        vgrid_b = Path.list_add(vgrid_a, (0, (lines - 0.5) * length))
+        if pattern_first_line == 'magic_ball' and pattern_last_line == 'magic_ball':
             grid[1] = [[vgrid_a[i], grid[1][i], vgrid_b[i]] if i % 2 == 0 else
                        [vgrid_b[i], grid[1][i], vgrid_a[i]] for i in range(len(grid[1]))]
+        elif pattern_first_line == 'magic_ball':
+            grid[1] = [[vgrid_a[i], grid[1][i]] if i % 2 == 0 else
+                       [grid[1][i], vgrid_a[i]] for i in range(len(grid[1]))]
+        elif pattern_last_line == 'magic_ball':
+            grid[1] = [[grid[1][i], vgrid_b[i]] if i % 2 == 0 else
+                       [vgrid_b[i], grid[1][i]] for i in range(len(grid[1]))]
 
         # create generic valley Path lines, one pointing up and other pointing down
         valley_types = [Path([(i * length / 2, (1 - i % 2) * length / 2) for i in range(2 * cols + 1)], 'v'),
@@ -86,14 +113,16 @@ class Waterbomb(Pattern):
         senses = np.array([bool((i % 2+i)/2 % 2) for i in range(2*lines)])
         if phase_shift:
             senses = np.invert(senses)
-        if pattern == "magic_ball":
+        if pattern_first_line == "magic_ball":
             senses[0] = ~senses[0]
+        if pattern_last_line == "magic_ball":
             senses[-1] = ~senses[-1]
         valleys = [valley_types[senses[i]] + (0, i * length / 2) for i in range(2*lines)]
 
         # convert first and last lines to mountains if magic_ball
-        if pattern == "magic_ball":
+        if pattern_first_line == "magic_ball":
             valleys[0].style = 'm'
+        if pattern_last_line == "magic_ball":
             valleys[-1].style = 'm'
 
         # invert every two lines to minimize laser cutter movements
