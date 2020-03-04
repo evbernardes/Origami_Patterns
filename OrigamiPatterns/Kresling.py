72b39a418e8f69c0230c106c4023c9cdde6ac160
evbernardes <s.evandro@hotmail.com>
2020-03-04 16:49:45 +0100

    Simplified code
    Signed-off-by: evbernardes <s.evandro@hotmail.com>

--- OrigamiPatterns/Kresling.py
+++ OrigamiPatterns/Kresling.py
@@ -104,52 +104,63 @@ class Kresling(Pattern):
         for i in range(sides + 1):
             for j in range(lines + 1):
                 if mirror_cells:
-                    vertices.append(Path((dx*((lines - j)%2) + a*i, dy * j), style='p', radius=vertex_radius))
+                    vertices.append(Path((dx*((lines - j)%2) + a*i, dy*j), style='p', radius=vertex_radius))
                 else:
                     vertices.append(Path((dx*(lines - j) + a*i, dy*j), style='p', radius=vertex_radius))
-        
+
         # create a horizontal grid, then offset each line according to angle
         grid_h = Path.generate_hgrid([0, a * sides], [0, dy * lines], lines, 'm')
-        grid_h_extended = Path.generate_hgrid([0, a * (sides+1)], [0, dy * lines], lines, 'm')
-        if mirror_cells:
+
+
+        if not mirror_cells:
+            # shift every mountain line of the grid to the right by increasing amounts
+            grid_h = Path.list_add(grid_h, [(i * dx, 0) for i in range(lines - 1, 0, -1)])
+        else:
+            # shift every OTHER mountain line of the grid a bit to the right
             grid_h = Path.list_add(grid_h, [((i%2)*dx, 0) for i in range(lines-1, 0, -1)])
             if add_attachment:
                 for i in range(lines%2, lines-1, 2):
+                    # hacky solution, changes length of every other mountain line
                     grid_h[i].points[1-i%2] = (grid_h[i].points[1-i%2][0] + a, grid_h[i].points[1-i%2][1])
-        else:
-            grid_h = Path.list_add(grid_h, [(i*dx, 0) for i in range(lines-1, 0, -1)])
 
+
+        # create MV zigzag for Kresling pattern
         zigzag = Kresling.generate_kresling_zigzag(sides, radius, angle_ratio, add_attachment)
-        zigzag_mirror = Path.list_reflect(zigzag, (0, (lines)*dy/2), (dx, (lines)*dy/2))
         zigzags = []
 
-        if mirror_cells:
+        # duplicate zigzag pattern for desired number of cells
+        if not mirror_cells:
+            for i in range(lines):
+                zigzags.append(Path.list_add(zigzag, (i * dx, (lines - i) * dy)))
+        else:
+            zigzag_mirror = Path.list_reflect(zigzag, (0, lines * dy / 2), (dx, lines * dy / 2))
             for i in range(lines):
                 if i % 2 == 1:
                     zigzags.append(Path.list_add(zigzag_mirror, (0, -(lines - i + (lines-1)%2) * dy)))
                 else:
-                    zigzags.append(Path.list_add(zigzag, (0, (lines - i ) * dy)))
+                    zigzags.append(Path.list_add(zigzag, (0, (lines - i) * dy)))
+
+        # create edge strokes
+        if not mirror_cells:
+            self.edge_points = [
+                (a * sides             , dy * lines),  # bottom right
+                (0                     , dy * lines),  # bottom left
+                (dx * lines            , 0),  # top left
+                (dx * lines + a * sides, 0)]  # top right
+
+            if add_attachment:
+                for i in range(lines):
+                    self.edge_points.append((dx * (lines - i) + a * (sides + 1), dy * i))
+                    if i != lines - 1:
+                        self.edge_points.append((dx * (lines - i - 1) + a * sides, dy * (i + 1)))
+
         else:
-            for i in range(lines):
-                zigzags.append(Path.list_add(zigzag, (i * dx, (lines - i) * dy)))
-
-
-        # # create a list for edge strokes
-        # self.edge_points = [
-        #     (dx*lines          , 0         ),   # top left
-        #     (dx*lines + a*sides, 0         ),   # top right
-        #     (a*sides           , dy*lines  ),   # bottom right
-        #     (0                 , dy*lines  )]  # bottom left
-
-        # create a list for edge strokes
-
-        if mirror_cells:
-            self.edge_points = [(a * sides + ((lines)%2)*dx , 0)]
+            self.edge_points = [(a * sides + (lines % 2)*dx, 0)]
 
             for i in range(lines+1):
-                self.edge_points.append([((lines+i)%2)*dx, dy*i])
+                self.edge_points.append([((lines+i) % 2)*dx, dy*i])
 
-            self.edge_points.append([a * sides + ((lines+i)%2)*dx, lines*dy])
+            self.edge_points.append([a * sides + ((lines+i) %2)*dx, lines*dy])
 
             if add_attachment:
                 for i in range(lines + 1):
@@ -157,24 +168,6 @@ class Kresling(Pattern):
             else:
                 for i in range(lines + 1):
                     self.edge_points.append([a*sides + (i%2)*dx, dy*(lines - i)])
-
-
-
-            # self.edge_points.append([(0, dy * lines)])
-
-        else:
-            self.edge_points = [
-                (a*sides               , dy*lines  ),   # bottom right
-                (0                     , dy*lines  ),   # bottom left
-                (dx * lines            , 0),            # top left
-                (dx * lines + a * sides, 0)]            # top right
-
-            if add_attachment:
-                for i in range(lines):
-                    self.edge_points.append((dx*(lines - i    ) + a*(sides + 1), dy*i      ))
-                    if i != lines - 1:
-                        self.edge_points.append((dx*(lines - i - 1) + a*(sides)    , dy*(i + 1)))
-
 
         self.path_tree = [grid_h, zigzags, vertices]
 
